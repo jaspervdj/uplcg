a3442974e9adb0eb95882a31aba1e3566426138c
Jasper Van der Jeugt <m@jaspervdj.be>
2020-08-25 12:45:57 +0200

    Last minute scripts/create-rooms.sh changes

--- scripts/create-rooms.sh
+++ scripts/create-rooms.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
 set -o nounset -o pipefail -o errexit
 
-for ((r=1; r <= 20; r++)); do
+for ((r=0; r <= 20; r++)); do
     echo $r
     curl 'https://uplcg.jaspervdj.be/rooms' \
         -H 'Content-Type: application/x-www-form-urlencoded' \
-        --data-raw "id=room$(printf "%02d" "$r")&password=&deck=icfp2020"
+        --data-raw "id=zoom$(printf "%02d" "$r")&password=&deck=icfp2020"
+done
+
+for ((r=1; r <= 10; r++)); do
+    echo $r
+    curl 'https://uplcg.jaspervdj.be/rooms' \
+        -H 'Content-Type: application/x-www-form-urlencoded' \
+        --data-raw "id=clowdr$(printf "%02d" "$r")&password=&deck=icfp2020"
 done
