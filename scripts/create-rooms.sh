648c6a17baef0237a2a991ae0fe983a3634be82d
Jasper Van der Jeugt <m@jaspervdj.be>
2020-09-11 16:50:35 +0200

    Bump script for munihac2020

--- scripts/create-rooms.sh
+++ scripts/create-rooms.sh
@@ -1,16 +1,21 @@
 #!/usr/bin/env bash
 set -o nounset -o pipefail -o errexit
 
-for ((r=0; r <= 20; r++)); do
-    echo $r
-    curl 'https://uplcg.jaspervdj.be/rooms' \
-        -H 'Content-Type: application/x-www-form-urlencoded' \
-        --data-raw "id=zoom$(printf "%02d" "$r")&password=&deck=icfp2020"
-done
+ROOMS=(
+    monoidalmu
+    loopinglambda
+    phantomphi
+    pointfreepsi
+    typedtheta
+    differentialdelta
+    covariantkappa
+    bifunctorialbeta
+    existentialeta
+    universalupsilon
+)
 
-for ((r=1; r <= 10; r++)); do
-    echo $r
-    curl 'https://uplcg.jaspervdj.be/rooms' \
-        -H 'Content-Type: application/x-www-form-urlencoded' \
-        --data-raw "id=clowdr$(printf "%02d" "$r")&password=&deck=icfp2020"
+for r in ${ROOMS[@]}; do
+     curl -i 'https://uplcg.jaspervdj.be/rooms' \
+         -H 'Content-Type: application/x-www-form-urlencoded' \
+         --data-raw "id=$r&password=&deck=munihac2020"
 done
